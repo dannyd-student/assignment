a082c8c7fea845380b201350d030f4f088f329ab
dannyd-student <dannydegraff05@gmail.com>
2026-04-07 21:17:55 -0700

    Update words.py

--- CISC179/save-the-watermelon/src/words.py
+++ CISC179/save-the-watermelon/src/words.py
@@ -1,1 +1,16 @@
+import random
 
+def get_word():
+    """
+    Returns a random word from a predefined list.
+    """
+    words = [
+        "apple",
+        "melon",
+        "grape",
+        "peach",
+        "banana",
+        "orange",
+        "cherry"
+    ]
+    return random.choice(words)
